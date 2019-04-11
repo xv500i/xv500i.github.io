90997c428052937725811ecb5a4907aac24d4d9b
Alex Soms <xv500i@gmail.com>
2019-04-11 19:13:13 +0200

    Calcula la puntuacio del jugador

--- src/Jugador.js
+++ src/Jugador.js
@@ -1,5 +1,11 @@
+
+
 function Jugador() {
     this.atac = 0;
     this.defensa = 0;
     this.assistencia = 0;
 }
+
+Jugador.prototype.CalculaPuntuacio = function() {
+    return this.atac + this.defensa + this.assistencia;
+};
--- tests/spec/JugadorSpec.js
+++ tests/spec/JugadorSpec.js
@@ -1,3 +1,77 @@
 describe("Jugador", function() {
+    var jugador;
 
+    beforeEach(function(){
+        jugador = new Jugador();
+    });
+
+    describe("Te valors de", function(){
+        it("atac", function(){
+            expect(jugador.atac).toBeDefined();
+        });
+    
+        it("defensa", function(){
+            expect(jugador.defensa).toBeDefined();
+        });
+    
+        it("assistencia", function(){
+            expect(jugador.assistencia).toBeDefined();
+        });
+    });
+
+    describe("Puntuacio", function(){
+        beforeEach(function(){
+            jugador.atac = 0;
+            jugador.defensa = 0;
+            jugador.assistencia = 0;
+        });
+
+        it("es mes gran quan mes atac te", function(){
+            let p1 = jugador.CalculaPuntuacio();
+            jugador.atac += 1;
+            let p2 = jugador.CalculaPuntuacio();
+            expect(p2).toBeGreaterThan(p1);
+        });
+
+        it("es mes gran quan mes defensa te", function(){
+            let p1 = jugador.CalculaPuntuacio();
+            jugador.defensa += 1;
+            let p2 = jugador.CalculaPuntuacio();
+            expect(p2).toBeGreaterThan(p1);
+        });
+
+        it("es mes gran quan mes assistencia te", function(){
+            let p1 = jugador.CalculaPuntuacio();
+            jugador.assistencia += 1;
+            let p2 = jugador.CalculaPuntuacio();
+            expect(p2).toBeGreaterThan(p1);
+        });
+
+        it("l'atac es igual d'important que la defensa", function(){
+            jugador.defensa += 1;
+            let p1 = jugador.CalculaPuntuacio();
+            jugador.defensa -= 1;
+            jugador.atac += 1;
+            let p2 = jugador.CalculaPuntuacio();
+            expect(p2).toBe(p1);
+        });
+
+        it("l'atac es igual d'important que la assistencia", function(){
+            jugador.atac += 1;
+            let p1 = jugador.CalculaPuntuacio();
+            jugador.atac -= 1;
+            jugador.assistencia += 1;
+            let p2 = jugador.CalculaPuntuacio();
+            expect(p2).toBe(p1);
+        });
+
+        it("la defensa es igual d'important que la assistencia", function(){
+            jugador.defensa += 1;
+            let p1 = jugador.CalculaPuntuacio();
+            jugador.defensa -= 1;
+            jugador.assistencia += 1;
+            let p2 = jugador.CalculaPuntuacio();
+            expect(p2).toBe(p1);
+        });
+    });
 });
