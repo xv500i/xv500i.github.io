6a4a10d146e32999942b14c59d9aec16beff0f96
Alex <xv500i@gmail.com>
2022-10-24 02:11:34 +0200

    implement weighted rooms

--- js/scripts.js
+++ js/scripts.js
@@ -9,34 +9,34 @@ const monsterRoomDescription = "Monster";
 const emptyRoomDescription = "Empty";
 function generate() {
     clearGenerated();
-    const trapRooms = elementIsChecked("room_types_trap");
-    const monsterRooms = elementIsChecked("room_types_monter");
-    const emptyRooms = elementIsChecked("room_types_empty");
+    const trapRoomsWeight = Number(getSelectedOption("room_types_trap"));
+    const monsterRoomsWeight = Number(getSelectedOption("room_types_monster"));
+    const emptyRoomsWeight = Number(getSelectedOption("room_types_empty"));
     const minLevel = Number(getSelectedOption("min_level"));
     const maxLevel = Number(getSelectedOption("max_level"));
     const minRooms = Number(getSelectedOption("min_rooms"));
     const maxRooms = Number(getSelectedOption("max_rooms"));
-    addGeneratedText(`Trap rooms: ${trapRooms}`);
-    addGeneratedText(`Monster rooms: ${monsterRooms}`);
-    addGeneratedText(`Empty rooms: ${emptyRooms}`);
+    addGeneratedText(`Trap rooms weigth: ${trapRoomsWeight}`);
+    addGeneratedText(`Monster rooms weigth: ${monsterRoomsWeight}`);
+    addGeneratedText(`Empty rooms weigth: ${emptyRoomsWeight}`);
     addGeneratedText(`Min level: ${minLevel}`);
     addGeneratedText(`Max level: ${maxLevel}`);
     addGeneratedText(`Min rooms: ${minRooms}`);
     addGeneratedText(`Max rooms: ${maxRooms}`);
     const rooms = getRandomInteger(minRooms, maxRooms);
     addGeneratedText(`Rooms: ${rooms}`);
-    let roomOptions = [];
-    if (trapRooms) {
-        roomOptions.push(RoomOption.TrapRoom);
-    }
-    if (monsterRooms) {
-        roomOptions.push(RoomOption.MonsterRoom);
-    }
-    if (emptyRooms) {
-        roomOptions.push(RoomOption.EmptyRoom);
-    }
+    let roomOptions = [
+        RoomOption.EmptyRoom,
+        RoomOption.MonsterRoom,
+        RoomOption.TrapRoom,
+    ];
+    let roomOptionsWeights = [
+        emptyRoomsWeight,
+        monsterRoomsWeight,
+        trapRoomsWeight,
+    ];
     for (let index = 1; index <= rooms; index++) {
-        let room = roomOptions[getRandomInteger(0, roomOptions.length - 1)];
+        let room = weightedRandom(roomOptions, roomOptionsWeights);
         let roomDescription = "";
         switch (room) {
             case RoomOption.TrapRoom:
@@ -75,5 +75,39 @@ function getGeneratedElement() {
 }
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function updateSliderText(id, value) {
+    document.getElementById(id).textContent = value.toString();
+}
+function weightedRandom(items, weights) {
+    if (items.length !== weights.length) {
+        throw new Error("Items and weights must be of the same size");
+    }
+    if (!items.length) {
+        throw new Error("Items must not be empty");
+    }
+    // Preparing the cumulative weights array.
+    // For example:
+    // - weights = [1, 4, 3]
+    // - cumulativeWeights = [1, 5, 8]
+    const cumulativeWeights = [];
+    for (let i = 0; i < weights.length; i += 1) {
+        cumulativeWeights[i] = weights[i] + (cumulativeWeights[i - 1] || 0);
+    }
+    // Getting the random number in a range of [0...sum(weights)]
+    // For example:
+    // - weights = [1, 4, 3]
+    // - maxCumulativeWeight = 8
+    // - range for the random number is [0...8]
+    const maxCumulativeWeight = cumulativeWeights[cumulativeWeights.length - 1];
+    const randomNumber = maxCumulativeWeight * Math.random();
+    // Picking the random item based on its weight.
+    // The items with higher weight will be picked more often.
+    for (let itemIndex = 0; itemIndex < items.length; itemIndex += 1) {
+        if (cumulativeWeights[itemIndex] >= randomNumber) {
+            return items[itemIndex];
+        }
+    }
+    return null;
 }
 //# sourceMappingURL=scripts.js.map
--- src/scripts.ts
+++ src/scripts.ts
@@ -10,17 +10,17 @@ const emptyRoomDescription = "Empty";
 
 function generate(): void {
   clearGenerated();
-  const trapRooms = elementIsChecked("room_types_trap");
-  const monsterRooms = elementIsChecked("room_types_monter");
-  const emptyRooms = elementIsChecked("room_types_empty");
+  const trapRoomsWeight = Number(getSelectedOption("room_types_trap"));
+  const monsterRoomsWeight = Number(getSelectedOption("room_types_monster"));
+  const emptyRoomsWeight = Number(getSelectedOption("room_types_empty"));
   const minLevel = Number(getSelectedOption("min_level"));
   const maxLevel = Number(getSelectedOption("max_level"));
   const minRooms = Number(getSelectedOption("min_rooms"));
   const maxRooms = Number(getSelectedOption("max_rooms"));
 
-  addGeneratedText(`Trap rooms: ${trapRooms}`);
-  addGeneratedText(`Monster rooms: ${monsterRooms}`);
-  addGeneratedText(`Empty rooms: ${emptyRooms}`);
+  addGeneratedText(`Trap rooms weigth: ${trapRoomsWeight}`);
+  addGeneratedText(`Monster rooms weigth: ${monsterRoomsWeight}`);
+  addGeneratedText(`Empty rooms weigth: ${emptyRoomsWeight}`);
   addGeneratedText(`Min level: ${minLevel}`);
   addGeneratedText(`Max level: ${maxLevel}`);
   addGeneratedText(`Min rooms: ${minRooms}`);
@@ -29,19 +29,19 @@ function generate(): void {
   const rooms = getRandomInteger(minRooms, maxRooms);
   addGeneratedText(`Rooms: ${rooms}`);
 
-  let roomOptions: RoomOption[] = [];
-  if (trapRooms) {
-    roomOptions.push(RoomOption.TrapRoom);
-  }
-  if (monsterRooms) {
-    roomOptions.push(RoomOption.MonsterRoom);
-  }
-  if (emptyRooms) {
-    roomOptions.push(RoomOption.EmptyRoom);
-  }
+  let roomOptions: RoomOption[] = [
+    RoomOption.EmptyRoom,
+    RoomOption.MonsterRoom,
+    RoomOption.TrapRoom,
+  ];
+  let roomOptionsWeights = [
+    emptyRoomsWeight,
+    monsterRoomsWeight,
+    trapRoomsWeight,
+  ];
 
   for (let index = 1; index <= rooms; index++) {
-    let room = roomOptions[getRandomInteger(0, roomOptions.length - 1)];
+    let room = weightedRandom(roomOptions, roomOptionsWeights);
     let roomDescription = "";
     switch (room) {
       case RoomOption.TrapRoom:
@@ -86,4 +86,41 @@ function getGeneratedElement(): HTMLElement {
 
 function getRandomInteger(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function updateSliderText(id: string, value: number) {
+  document.getElementById(id).textContent = value.toString();
+}
+
+function weightedRandom<T>(items: Array<T>, weights: Array<number>): T {
+  if (items.length !== weights.length) {
+    throw new Error("Items and weights must be of the same size");
+  }
+  if (!items.length) {
+    throw new Error("Items must not be empty");
+  }
+  // Preparing the cumulative weights array.
+  // For example:
+  // - weights = [1, 4, 3]
+  // - cumulativeWeights = [1, 5, 8]
+  const cumulativeWeights = [];
+  for (let i = 0; i < weights.length; i += 1) {
+    cumulativeWeights[i] = weights[i] + (cumulativeWeights[i - 1] || 0);
+  }
+  // Getting the random number in a range of [0...sum(weights)]
+  // For example:
+  // - weights = [1, 4, 3]
+  // - maxCumulativeWeight = 8
+  // - range for the random number is [0...8]
+  const maxCumulativeWeight = cumulativeWeights[cumulativeWeights.length - 1];
+  const randomNumber = maxCumulativeWeight * Math.random();
+  // Picking the random item based on its weight.
+  // The items with higher weight will be picked more often.
+  for (let itemIndex = 0; itemIndex < items.length; itemIndex += 1) {
+    if (cumulativeWeights[itemIndex] >= randomNumber) {
+      return items[itemIndex];
+    }
+  }
+
+  return null;
 }
