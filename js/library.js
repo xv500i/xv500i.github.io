8a1d58b0d04ca5f1d5eb5d7c4aedbe07c0a1cdd1
Alex <xv500i@gmail.com>
2025-08-01 16:14:23 +0200

    switch to calculator

--- js/library.js
+++ js/library.js
@@ -1,6 +1,43 @@
-function onReady() {
-  const placeholder = $("#placeholder");
+var totalSpan = $("#total");
+var waterKey = $("#keys-water");
+var waterAmount = 0;
+var sodaKey = $("#keys-soda");
+var sodaAmount = 0;
+var sandWitchKey = $("#keys-sandwitch");
+var sandWitchAmount = 0;
+var total = 0;
 
-  prepareFilter(placeholder, bestiaryData);
-  prepareMonsters(placeholder, bestiaryData);
+function addItem(name) {
+  switch (name) {
+    case "water":
+      waterAmount++;
+      total += 1;
+      break;
+    case "soda":
+      sodaAmount++;
+      total += 3;
+      break;
+    case "sandWitch":
+      sandWitchAmount++;
+      total += 5;
+      break;
+  }
+  updateUI();
+}
+
+function reset() {
+  total = 0;
+  waterAmount = 0;
+  sodaAmount = 0;
+  sandWitchAmount = 0;
+
+  updateUI();
+}
+
+function updateUI() {
+  totalSpan.text(total + "€");
+
+  waterKey.children(".amount").text(waterAmount);
+  sodaKey.children(".amount").text(sodaAmount);
+  sandWitchKey.children(".amount").text(sandWitchAmount);
 }
